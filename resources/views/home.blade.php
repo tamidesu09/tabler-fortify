3e6246d8d4a696477b372c88a11d94928550c571
= <=>
2023-06-26 19:24:09 +0800

    working datatable

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -490,6 +490,7 @@
             </table>
         </div>
     </div>
+    
 </div>
 @endsection
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,7 @@
     </div>
 
     <script src="https://unpkg.com/@adminkit/core@latest/dist/js/app.js"></script>
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.datatables.net/v/ju/dt-1.13.4/datatables.min.js"></script>
     @yield('scripts')
 
